f2e33fd1c4f465c2412be419d77b6bbdc2e2ab27
freakster00 <rohan7poudel@gmail.com>
2025-05-17 19:28:09 +0545

    chore(day-1): understanding basic concepts - chapter-00

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+fn main(){
+    println!("Guess the number game!!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
+    loop{
+            println!("Please input your guess: ");
+    let mut guess=String::new();
+    io::stdin().read_line(&mut guess).expect("Error occured");
+
+    println!("you guessed: {}",guess);
+    let guess:u32 = guess.trim().parse().expect("Please type a number!");
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => {
+            println!("you win!");
+            break;
+        }
+    }
+    }
+
+
+    // let x:u8=254;
+    // let y:u8=1;
+
+    // println!("x={} and y={} and x+y={}",x,y,x+y);
+
+
 }
